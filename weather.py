daf7cdd1b2384b7dc117bc24f6e3a886eab58c39
aparaxsarkar <81165861+aparaxsarkar@users.noreply.github.com>
2021-06-23 17:02:11 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,23 +2,23 @@ import requests
 from datetime import datetime
 
 api_key = 'ff1ea325d9615c351e65ddabbd6e3757'
-location = input("Enter the city name: ")
+loc = input("Enter the city name: ")
 
-complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+loc+"&appid="+api_key
 api_link = requests.get(complete_api_link)
 api_data = api_link.json()
 
-temp_city = ((api_data['main']['temp']) - 273.15)
-weather_desc = api_data['weather'][0]['description']
-hmdt = api_data['main']['humidity']
-wind_spd = api_data['wind']['speed']
+city = ((api_data['main']['temp']) - 273.15)
+weather_description = api_data['weather'][0]['description']
+humidity = api_data['main']['humidity']
+wind_speed = api_data['wind']['speed']
 date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
 print ("-------------------------------------------------------------")
-print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("Weather Stats for - {} | {}".format(loc.upper(), date_time))
 print ("-------------------------------------------------------------")
 
-print ("Current temperature is: {:.2f} deg C".format(temp_city))
-print ("Current weather desc  :",weather_desc)
-print ("Current Humidity      :",hmdt, '%')
-print ("Current wind speed    :",wind_spd ,'kmph')
+print ("Current temperature : {:.2f} deg C".format(city))
+print ("Weather description :",weather_description)
+print ("Humidity            :",humidity, '%')
+print ("Wind speed          :",wind_speed ,'kmph')
